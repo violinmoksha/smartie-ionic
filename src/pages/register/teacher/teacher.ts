b41b6774904e7b701dc2ad109c8ff5be17f6e214
paulsolutionfuse <paul@solutionfuse.com>
2017-12-11 21:19:31 -0800

    nope, turn on debug div

--- src/pages/register/teacher/teacher.ts
+++ src/pages/register/teacher/teacher.ts
@@ -23,12 +23,11 @@ export class RegisterTeacher {
       email: new FormControl('', Validators.required),
       username: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      confPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+      confPassword: new FormControl('', [Validators.required, Validators.minLength(6), this.equalTo('password')])
     });
 
   }
 
-/*
     equalTo(equalControlName): ValidatorFn {
       return (control: AbstractControl): {
         [key: string]: any
@@ -42,7 +41,7 @@ export class RegisterTeacher {
         };
       };
     }
-*/
+
 
   next(form1Value){
     this.navCtrl.push(RegisterTeacherStep2, { form1Value : form1Value });
