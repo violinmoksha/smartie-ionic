af110c107b50494322b210a255cfe2eb146aa3cf
paulsolutionfuse <paul@solutionfuse.com>
2018-01-06 15:57:13 -0800

    Task #39 begin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
 
 import { Payment } from '../pages/payment/payment';
 import { QrCodeScanner } from '../pages/qr-code-scanner/qr-code-scanner';
@@ -39,7 +40,7 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, private storage: Storage, private translate: TranslateService) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, private storage: Storage, private translate: TranslateService, private push: Push) {
     this.platform = platform;
     this.initializeApp();
 
@@ -66,6 +67,53 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      // init Push, see https://ionicframework.com/docs/native/push/
+      // to check if we have permission
+      this.push.hasPermission().then((res: any) => {
+        if (res.isEnabled) {
+          console.log('We have permission to send push notifications');
+        } else {
+          console.log('We do not have permission to send push notifications');
+        }
+      });
+
+      // Create a channel (Android O and above). You'll need to provide the id, description and importance properties.
+      this.push.createChannel({
+       id: "testchannel1",
+       description: "My first test channel",
+       // The importance property goes from 1 = Lowest, 2 = Low, 3 = Normal, 4 = High and 5 = Highest.
+       importance: 3
+      }).then(() => console.log('Channel created'));
+
+      // Delete a channel (Android O and above)
+      //this.push.deleteChannel('testchannel1').then(() => console.log('Channel deleted'));
+
+      // Return a list of currently configured channels
+      this.push.listChannels().then((channels) => console.log('List of channels', channels))
+
+      // to initialize push notifications
+
+      const options: PushOptions = {
+         android: { senderID: "smartie"},
+         ios: {
+             alert: 'true',
+             badge: true,
+             sound: 'false'
+         },
+         windows: {},
+         browser: {
+             pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+         }
+      };
+
+      const pushObject: PushObject = this.push.init(options);
+
+      pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+
+      pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+
+      pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
 
       //Parse.initialize("948b9456-8c0a-4755-9e84-71be3723d338", "49bc1a33-dfe7-4a32-bdcc-ee30b7ed8447");
       //Parse.serverURL = 'https://test.t0tl3s.com/parse';
