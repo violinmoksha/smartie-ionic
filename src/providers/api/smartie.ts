4b53da74069d1589e0d611af6037c92ed3098481
paulsolutionfuse <paul@solutionfuse.com>
2018-01-29 08:05:45 -0800

    additional chgs in smartie.ts

--- src/providers/api/smartie.ts
+++ src/providers/api/smartie.ts
@@ -12,32 +12,30 @@ import { Constants } from './constants';
 export class SmartieAPI {
   private baseUrl: string;
   private applicationId: string;
-  private masterKey: string;
   private contentType: string;
 
   constructor(public http: HttpClient) {
-
   }
 
   getApi(endPoint, body){
-    if(Constants.API_ENDPOINTS.env === 'local'){
-      this.baseUrl = Constants.API_ENDPOINTS.baseUrls.local;
-      this.applicationId = Constants.API_ENDPOINTS.headers.localAndTest.applicationId;
-      this.contentType = Constants.API_ENDPOINTS.headers.localAndTest.contentType;
-    }else if(Constants.API_ENDPOINTS.env === 'test'){
-      this.baseUrl = Constants.API_ENDPOINTS.baseUrls.test;
-      this.applicationId = Constants.API_ENDPOINTS.headers.localAndTest.applicationId;
-      this.contentType = Constants.API_ENDPOINTS.headers.localAndTest.contentType;
-    }else if(Constants.API_ENDPOINTS.env === 'prod'){
-      this.baseUrl = Constants.API_ENDPOINTS.baseUrls.prod;
-      this.applicationId = Constants.API_ENDPOINTS.headers.prod.applicationId;
-      this.contentType = Constants.API_ENDPOINTS.headers.prod.contentType;
+    const ourBaseUrls = Constants.API_ENDPOINTS.baseUrls;
+    const ourHeaders = Constants.API_ENDPOINTS.headers;
+    const ourEnv = Constants.API_ENDPOINTS.env;
+
+    if(ourEnv === 'local' || ourEnv === 'test'){
+      (ourEnv === 'test') ?
+        this.baseUrl = ourBaseUrls.test :
+        this.baseUrl = ourBaseUrls.local;
+      this.applicationId = ourHeaders.localAndTest.applicationId;
+    } else if(Constants.API_ENDPOINTS.env === 'prod'){
+      this.baseUrl = ourBaseUrls.prod;
+      this.applicationId = ourHeaders.prod.applicationId;
     }
+    this.contentType = 'application/json';
 
     const httpOptions = {
       headers: new HttpHeaders({
         'X-Parse-Application-Id': this.applicationId,
-        'X-Parse-Master-Key': this.masterKey,
         'Content-Type': this.contentType
       })
     };
