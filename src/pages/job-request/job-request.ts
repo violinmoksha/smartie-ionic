31f225549054f29df1aa5acf8b4a37fb610cba54
John Melchior <john@solutionfuse.com>
2018-02-09 20:34:27 +0530

    update SentJobRequest

--- src/pages/job-request/job-request.ts
+++ src/pages/job-request/job-request.ts
@@ -21,12 +21,11 @@ export class JobRequestPage {
   private body: any;
   private submitInProgress: boolean;
   private loading: any;
-  private fromWhere: any;
   private userRole: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public smartieApi: SmartieAPI, private storage: Storage, private loadingCtrl: LoadingController) {
     this.params = navParams.data.params;
-    //this.fromWhere = navParams.data.fromWhere;
+    console.log(this.params);
 
     this.submitInProgress = false;
     this.loading = this.loadingCtrl.create({
@@ -71,43 +70,41 @@ export class JobRequestPage {
     this.submitInProgress = true;
     this.loading.present();
 
-    this.storage.get('role').then(role => {
-      this.storage.get(role+"UserProfile").then(profile => {
-        if (role == 'teacher') {
-          this.body = {
-            requestingProfileId: this.params.requestedId,
-            requestedProfileId: JSON.parse(profile).profileData.objectId,
-            jobDescription: this.params.profileAbout,
-            prefLocation: this.params.prefLocation,
-            requestSent: true,
-            acceptedState: false
-          };
-        } else {
-          this.body = {
-            requestingProfileId: JSON.parse(profile).profileData.objectId,
-            requestedProfileId: this.params.requestedId,
-            jobDescription: this.params.profileAbout,
-            prefLocation: this.params.prefLocation,
-            requestSent: true,
-            acceptedState: false
-          };
-        }
+    this.storage.get("UserProfile").then(profile => {
+      if (profile.profileData.role == 'teacher') {
+        this.body = {
+          teacherProfileId: profile.profileData.objectId,
+          otherProfileId: this.params.otherProfileId,
+          jobDescription: this.params.profileAbout,
+          prefLocation: this.params.prefLocation,
+          requestSent: true,
+          acceptedState: false
+        };
+      } else {
+        this.body = {
+          teacherProfileId: this.params.teacherProfileId,
+          otherProfileId: profile.profileData.objectId,
+          jobDescription: this.params.profileAbout,
+          prefLocation: this.params.prefLocation,
+          requestSent: true,
+          acceptedState: false
+        };
+      }
 
-        let API = this.smartieApi.getApi(
-          'setJobRequest',
-          this.body
-        );
-        return new Promise(resolve => {
-          interface Response {
-            result: any
-          };
-          this.smartieApi.http.post<Response>(API.apiUrl, API.apiBody, API.apiHeaders ).subscribe(response => {
-            this.requestSent = true;
-            this.viewCtrl.dismiss();
-            this.loading.dismiss();
-            this.submitInProgress = false;
-          });
-        })
+      let API = this.smartieApi.getApi(
+        'setJobRequest',
+        this.body
+      );
+      return new Promise(resolve => {
+        interface Response {
+          result: any
+        };
+        this.smartieApi.http.post<Response>(API.apiUrl, API.apiBody, API.apiHeaders ).subscribe(response => {
+          this.requestSent = true;
+          this.viewCtrl.dismiss();
+          this.loading.dismiss();
+          this.submitInProgress = false;
+        });
       })
     })
   }
