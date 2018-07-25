cb827dc822ca788e2f96582d2ad454db0a0328a1
paulsolutionfuse <paul@solutionfuse.com>
2018-07-24 19:03:12 -0700

    feedback boilerplate, needs endpoint

---
+++ src/pages/feedback/feedback.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SmartieAPI } from '../../providers/api/smartie';
+import { Storage } from '@ionic/storage';
+
+
+/**
+ * Generated class for the SetReviewPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feedback',
+  templateUrl: 'feedback.html',
+})
+export class FeedbackPage {
+
+  profileData: any;
+  role: any;
+  feedback: any = '';
+  genericAvatar: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public smartieApi: SmartieAPI) {
+    this.profileData = navParams.get("profileData");
+
+    if (this.profileData.role == 'teacher') {
+      this.genericAvatar = '/assets/imgs/user-img-teacher.png';
+    } else if (this.profileData.role == 'student') {
+      this.genericAvatar = '/assets/imgs/user-img-student.png';
+    } else if (this.profileData.role == 'parent') {
+      this.genericAvatar = '/assets/imgs/user-img-parent.png';
+    } else if (this.profileData.role == 'school') {
+      this.genericAvatar = '/assets/imgs/user-img-school.png';
+    }
+
+    this.storage.get("UserProfile").then(profile => {
+      this.role = profile.profileData.role;
+    })
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SetReviewPage');
+  }
+
+  submitFeedback(){
+    let API = this.smartieApi.getApi(
+      'setFeedback',
+      { feedback: this.feedback, profileId: this.profileData.objectId }
+    );
+    interface Response {
+      result: any
+    }
+    this.smartieApi.http.post<Response>(API.apiUrl, API.apiBody, API.apiHeaders ).subscribe(response => {
+      console.log(response.result);
+    })*/
+
+  }
+
+}
