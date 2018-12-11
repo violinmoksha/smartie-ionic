6312521fa2120d2a215b6d0b0f92fa676d6eee5d
ramesh912 <ramesh@solutionfuse.com>
2018-12-11 17:14:34 +0530

    chat pagination n UI design changes n added date time info

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -33,13 +33,17 @@ export class ChatPage {
   receiver: any;
   chatRoom:any;
   userProfileId:String;
+  pageNumber = 0;
+  messageLimitPerPage: Number = 5;
+  roomId : String;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public events: Events, private dataService: DataService) {
     this.params = navParams.get("jobObject");
     this.sender = navParams.get("sender");
     this.receiver = navParams.get("receiver");
     this.navigateFrom = navParams.get("from");
-
-
+    this.pageNumber = 0;
+      
     if (this.navigateFrom == "jobrequest") {
       if (!this.params.teacherProfile.stripeCustomer || this.params.teacherProfile.stripeCustomer == 'undefined') {
         this.chatAccess = true;
@@ -51,32 +55,32 @@ export class ChatPage {
         this.teacherProfileId = this.params.objectId;
         this.studentProfileId = this.params.otherProfile.objectId;
       }
+      this.roomId = this.teacherProfileId + '|' + this.studentProfileId;
     }else{
       this.chatAccess = true;
       this.chatRoom = navParams.get('chat');
       this.receiver = this.chatRoom.receiver
+      this.roomId = this.chatRoom.roomId;
+      for (var k = 0; k < this.chatRoom.messages.length; k++) {
+        if (k == 0)
+          this.chatRoom.messages[k].displayTime = this.getSentTime(this.chatRoom.messages[k].sentAt, this.chatRoom.messages[k].sentAt);
+        else
+          this.chatRoom.messages[k].displayTime = this.getSentTime(this.chatRoom.messages[k].sentAt, this.chatRoom.messages[k - 1].sentAt);
+      }
       this.chatMessages = this.chatRoom.messages.reverse();
     }
-
 
     this.storage.get("UserProfile").then(profile => {
       this.role = profile.profileData.role;
       this.userId = profile.userData.objectId;
       this.userProfileId = profile.profileData.objectId;
     })
-
-    //this.scrollto();
   }
 
-  onScroll(scrollEvent) {
-    // if (scrollEvent.scrollTop == 0) {
-    //   console.log(this.allmessages.length);
-    //   console.log("Almost reach top");
-    //   this.getAllMessages(this.allmessages.length);
-    // }
-  }
 
-  ionViewDidLoad() {
+  fetchMore(infiniteScroll) {
+    this.pageNumber += 1;
+    this.fetchMessages(infiniteScroll);
   }
 
   ionViewDidEnter() {
@@ -129,64 +133,92 @@ export class ChatPage {
     this.dataService.getApi('sendMessage', messageBody).then(API => {
       this.dataService.httpPost(API['apiUrl'], API['apiBody'], API['apiHeaders']).then(res => {
         console.log(res);
+        res.result.messages[0] = this.getSentTime(res.result.messages[0].sentAt, new Date().toISOString())
         this.chatMessages.push(res.result.messages[0]);
         this.newmessage = '';
       })
     })
   }
 
-  addmessage() {
+  // addmessage() {
+  //   this.dataService.getApi(
+  //     'addChatMessage',
+  //     { teacherProfileId: this.teacherProfileId, studentProfileId: this.studentProfileId, message: this.newmessage, viewed: false, role: this.role }
+  //   ).then(async API => {
+  //     return await this.dataService.httpPost(API['apiUrl'], API['apiBody'], API['apiHeaders']).then(async response => {
+  //       this.content.scrollToBottom();
+  //       this.newmessage = '';
+  //       console.log(response);
+  //     }, err => {
+  //       console.log(err);
+  //     })
+  //   });
+  // }
+
+  // scrollto() {
+  //   setTimeout(() => {
+  //     console.log("Scroll to bottom");
+  //     this.content.scrollToBottom();
+  //   }, 1000);
+  // }
+
+  fetchMessages(infiniteScroll) {
+
     this.dataService.getApi(
-      'addChatMessage',
-      { teacherProfileId: this.teacherProfileId, studentProfileId: this.studentProfileId, message: this.newmessage, viewed: false, role: this.role }
-    ).then(async API => {
-      return await this.dataService.httpPost(API['apiUrl'], API['apiBody'], API['apiHeaders']).then(async response => {
-        this.content.scrollToBottom();
-        this.newmessage = '';
-        console.log(response);
-        this.getAllMessages();
+      'fetchMessages',
+      { roomId: this.roomId, page: this.pageNumber, limit: this.messageLimitPerPage }
+    ).then(API => {
+      this.dataService.httpPost(API['apiUrl'], API['apiBody'], API['apiHeaders']).then(res => {
+        console.log(res);
+        infiniteScroll.complete();
+        if (res.result[0].messages.length > 0) {
+          for (var i = 0; i < res.result[0].messages.length; i++) {
+            if(i == 0)
+            res.result[0].messages[i].displayTime = this.getSentTime(res.result[0].messages[i].sentAt, res.result[0].messages[i].sentAt)
+            else
+            res.result[0].messages[i].displayTime = this.getSentTime(res.result[0].messages[i].sentAt, res.result[0].messages[i - 1].sentAt)
+
+            this.chatMessages.splice(0, 0, res.result[0].messages[i]);
+          }
+          if (res.result[0].messages.length < this.messageLimitPerPage) {
+            infiniteScroll.enable(false);
+            console.log("message finished")
+          }
+        } else {
+          console.log(res)
+        }
       }, err => {
         console.log(err);
+        infiniteScroll.complete();
       })
-    });
-  }
-
-  scrollto() {
-    setTimeout(() => {
-      console.log("Scroll to bottom");
-      this.content.scrollToBottom();
-    }, 1000);
-  }
-
-  getAllMessages(skip = null) {
-    console.log(this.studentProfileId);
-    console.log(this.teacherProfileId);
-
-    return new Promise(async (resolve) => {
-      return await this.dataService.getApi(
-        'getAllMessages',
-        { teacherProfileId: this.teacherProfileId, studentProfileId: this.studentProfileId, skip: skip }
-      ).then(async API => {
-        return await this.dataService.httpPost(API['apiUrl'], API['apiBody'], API['apiHeaders']).then(async response => {
-          this.chatMessages = [];
-          for (let chat of response.result) {
-            this.chatMessages.push(chat);
-          }
-
-          if (skip) {
-            console.log("append chat");
-            this.allmessages.concat(this.chatMessages);
-            console.log(this.allmessages);
-          } else {
-            console.log('new message');
-            this.allmessages = this.chatMessages;
-            console.log(this.allmessages);
-          }
-
-        }, err => {
-          console.log(err);
-        })
-      });
     })
   }
+
+  getSentTime(currentIso, previousIso) {
+    let sentDate = new Date(currentIso);
+    let prevDate = new Date(previousIso);
+    let sentTime = sentDate.getHours() + ':' + sentDate.getMinutes();
+    let messageDate = null;
+    let monthNames = ["Jan", "Feb", "Mar", "April", "May", "June",
+      "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    function getMessageDate(){
+      console.log(sentDate.getMonth());
+      console.log(monthNames)
+      return sentDate.getDate() +' '+ monthNames[10] +' '+ sentDate.getFullYear();
+    }
+    if (prevDate.getDate() < sentDate.getDate()) {
+      messageDate = getMessageDate();
+    } else if (prevDate.getDate() < sentDate.getDate()) {
+      messageDate = getMessageDate();
+    } else if (prevDate.getFullYear() < sentDate.getFullYear()) {
+      messageDate = getMessageDate();
+    } else {
+      messageDate = null;
+    }
+    console.log(messageDate);
+    return { sentTime, messageDate };
+  }
+
+  
 }
