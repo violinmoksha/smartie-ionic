18d4677f161db19949b814fd8dffd863dae50a09
ramesh912 <ramesh@solutionfuse.com>
2019-05-15 19:27:43 +0530

    available timings fix

--- src/pages/myprofile/myprofile.ts
+++ src/pages/myprofile/myprofile.ts
@@ -34,11 +34,26 @@ export class MyprofilePage {
         let levels:any = this.utils.getProfLevelByActive(userData.profileData.badgeName, this.role);
         console.log(levels)
         this.nextLevel = levels.next;
-        this.prevLevel = levels.prev
-      }
+        this.prevLevel = levels.prev;
 
+        }
+        if(userData && userData.specificUser){
+          this.user.specificUser = userData.specificUser;
+          this.user.specificUser.UTCStartTime = this.getTimeString(userData.specificUser.defaultStartDateTime.iso);
+          this.user.specificUser.UTCEndTime = this.getTimeString(userData.specificUser.defaultEndDateTime.iso);
+          this.user.specificUser.UTCstartDate = new Date(userData.specificUser.defaultStartDateTime.iso).toLocaleDateString();
+          this.user.specificUser.UTCendDate = new Date(userData.specificUser.defaultEndDateTime.iso).toLocaleDateString();
+      }
       this.timeZone = new Date().toString().match(/\(([A-Za-z\s].*)\)/)[1];
     })
+  }
+
+  getTimeString(date) {
+    var dt = new Date(date);
+    var time = dt.toLocaleTimeString();
+    time = time.replace(/\u200E/g, '');
+    time = time.replace(/^([^\d]*\d{1,2}:\d{1,2}):\d{1,2}([^\d]*)$/, '$1$2');
+    return time;
   }
 
   ionViewDidLoad() {
